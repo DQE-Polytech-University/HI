1140638b2e36db88afdd0f696507b0a84e6a28e8
Alena Popova <Popova-alena1995@mail.ru>
2016-05-09 23:27:56 +0400

    1 m v

--- src/Particle.py
+++ src/Particle.py
@@ -6,6 +6,7 @@ Created on Thu May  5 14:50:51 2016
 """
 import numpy as np
 import math
+
 class Particle:
     def __init__(self,bas,n_pol): #selected number of bases and polarizations
         self.set_numberbas=bas #is the number of bases
@@ -17,47 +18,45 @@ class Particle:
         a=self.set_numberbas
         if a==1:
             pol=np.random.choice([0,90])
-            qubit=[[math.cos(pol)][math.sin(pol)]]
+            self.qubit=[[math.cos(pol)][math.sin(pol)]]
         elif a==2:
             if n_bas==0:
                 Pol=np.random.choice([0,90])
                 if Pol==0:
-                    qubit=[[1,0],[1/math.sqrt(2),-1/math.sqrt(2)]]
+                    self.qubit=[[1,0],[1/math.sqrt(2),-1/math.sqrt(2)]]
+                 #   self.qubit1=[[float(math.cos(Pol/2))][float(math.sin(Pol/2))]]
                 else:
-                    qubit=[[0,1],[1/math.sqrt(2),1/math.sqrt(2)]]
+                    self.qubit=[[0,1],[1/math.sqrt(2),1/math.sqrt(2)]]
+                  #  self.qubit1=[[math.sin(Pol/2)][float(-math.cos(Pol/2))]]
             else:
                 Pol=np.random.choice([45,135])
                 if Pol==45:
-                    qubit=[[1/math.sqrt(2),1/math.sqrt(2)],[1,0]]
+                    self.qubit=[[1/math.sqrt(2),1/math.sqrt(2)],[1,0]]
+                  #  self.qubit1=[[math.cos(Pol/2)][-math.sin(Pol/2)]]
                 else:
-                    qubit=[[-1/math.sqrt(2),1/math.sqrt(2)],[0,1]]
+                    self.qubit=[[-1/math.sqrt(2),1/math.sqrt(2)],[0,1]]
+                 #   self.qubit1=[[math.sin(Pol/2)][math.cos(Pol/2)]]
         else:
             if n_bas==0:
                 pol=np.random.choice([0,90])
                 if Pol==0:
-                    qubit=[[1,0],[1/math.sqrt(2),-1/math.sqrt(2)]]
+                    self.qubit=[[1,0],[1/math.sqrt(2),-1/math.sqrt(2)]]
                 else:
-                    qubit=[[0,1],[1/math.sqrt(2),1/math.sqrt(2)]]
+                    self.qubit=[[0,1],[1/math.sqrt(2),1/math.sqrt(2)]]
             elif n_bas==1:
                 Pol=np.random.choice([45,135])
                 if Pol==45:
-                    qubit=[[1/math.sqrt(2),1/math.sqrt(2)],[1,0]]
+                    self.qubit=[[1/math.sqrt(2),1/math.sqrt(2)],[1,0]]
                 else:
-                    qubit=[[-1/math.sqrt(2),1/math.sqrt(2)],[0,1]]
+                    self.qubit=[[-1/math.sqrt(2),1/math.sqrt(2)],[0,1]]
             else:
                 Pol=np.random.choice([90,-90])
                 if Pol==45:
-                    qubit=[[1/math.sqrt(2),1/math.sqrt(2)],[1,0]]
+                    self.qubit=[[1/math.sqrt(2),1/math.sqrt(2)],[1,0]]
                 else:
-                    qubit=[[-1/math.sqrt(2),1/math.sqrt(2)],[0,1]]
+                    self.qubit=[[-1/math.sqrt(2),1/math.sqrt(2)],[0,1]]
+        print('my qubit',self.qubit)
+q=Particle(2,4)
+q.formation(1)
+
        
-       
-       
-       
-       
-       self.basis=self.set_numberbas
-        if self.polarization==1:
-            if self.basis==1:
-                a=np.random.randint(0,2)
-                b=np.random.randint(0,2)
-                self.qubit=[[a],[b]]
