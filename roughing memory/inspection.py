825b274b630cf3be15c9616131c7a6a6f4cacaaf
Alena Popova <Popova-alena1995@mail.ru>
2016-05-21 02:41:29 +0400

    Другая структура наследования

--- roughing memory/inspection.py
+++ roughing memory/inspection.py
@@ -7,26 +7,102 @@ Created on Sun May 15 20:49:16 2016
 
 import numpy as np
 import math
-from cmath import sin, cos, e
+from cmath import *
 
 class Qubit:
+    def __init__(self,alpha = None,theta = None):
+        self.alpha = alfa
+        self.theta = theta
+        self.qubit =  cos()+sin() 
     
-    def Polarization(self,*args):
-        self.tuple_pol=tuple(args)
-        self.len_pol=len(self.tuple_pol)
+    def __repr__(self):
+        return "qubit( " + str(self.alpha)+str(self.theta)+")"
+        
+    def generate_one_qubit
+        
+
+
+class Generator():
+    
+    def __init__(self, quantity_bas=None,long_message):
+        self.long_message =long_message
+        self.quantity_bas = quantity_bas
+        
+    def write_polarization(self,*args):
+        self.write_pol=tuple(args)
+        self.len_pol=len(self.write_pol)
         return args
+    
+    def write_bases(self,*args):
+        self.write_bas = tuple(args)
+        self.len_bas = len(self.write_bas)
+        return args
+
+    def send(self)
+        self.__randomly_bases=np.random.choice(self.write_bas,int(self.long_message),True,None)
+        self.__randomly_polarization=np.random.choice(self.write_pol,int(self.long_message),True,None)
         
-    def Keyon(self, r):
-        self.taken_qubits=r
+    
+    
+    
+Class Alisa(Generator):
+	def __init__(self):
+        Generator.__init__(self)
+        Generator.send(self)
+        
+    def get_randomly_bases(self):
+        return self.__randomly_bases
+        
+    def get_randomly_polarization(self):
+        return self.__randomly_polarization
+        
+Class Bob(Generator):
+				def __init__(self):
+        Generator.__init__(self)
+        Generator.send(self)
+        
+    def get_randomly_bases(self):
+        return self.__randomly_bases
+        
+    def get_randomly_polarization(self):
+        return self.__randomly_polarization
+        
+        
+Class Various_measurement():
+
+				def __init__(self):
+					q=Generator()
+     alica=Alisa()
+     boby=Bob()
+			  qubit=
+    def compare_Bob_Alise(self):
+        self.taken_qubits=[]
+        i=0
+        while i<int(q.long_message):
+            if alica.randomly_bases[i]==boby.randomly_bases[i]:
+                self.taken_qubits.append(alica.randomly_polarization(i))
+            else:
+                taken_qubits.append(boby.randomly_polarization(i))
+        return self.taken_qubits
+        
+    def generate_key(self):
         self.key=[]
-        if (-90)<=self.taken_qubits<90:
-            self.key.append(1)
-        else:
-            self.key.append(0)
-        
+        i=0
+        while i<int(self.set_long):
+            if alica.randomly_bases[i]==boby.randomly_bases[i]:
+                if (-90)<=self.taken_qubits[i]<90:
+                    self.key.append(1)
+                else:
+                    self.key.append(0)
+            else:
+                pass
+                
+                
+    
+    				
     
     
-q=Qubit()
-q.Polarization(0,45,90,135)
-q.Keyon(-99)
-print(q.key)
+    
+    
+    
+    
--- src/Particle.py
+++ src/Particle.py
@@ -6,17 +6,86 @@ Created on Thu May  5 14:50:51 2016
 """
 import numpy as np
 import math
-from cmath import sin, cos, e
 
 class Qubit:
     
-    def __init__(self,polariz=None,circular=None):
-        self.alfa = polariz
-        self.theta = circular  
+    def __init__(self,alpha=None,theta=None):
+        self.alpha = alpha
+        self.theta = theta  
+        self.qubit = math.cos(alpha)+(math.e**(math.j*theta))*math.sin(alpha)
     
     def __repr__(self):
         return "qubit: " + str(self.qubit)
         
+
+
+
+class Generator():
+    def __init__(self, quantity_bas,long_message):
+        self.long_message =long_message
+        self.quantity_bas = quantity_bas
+        
+    def write_polarization(self,*args):
+        self.write_pol=tuple(args)
+        self.len_pol=len(self.write_pol)
+        return args
+    
+    def write_bases(self,*args):
+        self.write_bas = tuple(args)
+        self.len_bas = len(self.write_bas)
+        return args
+
+    def send(self):
+        self.randomly_bases=np.random.choice(self.write_bas,self.long_message)
+        self.randomly_polarization=np.random.choice(self.write_pol,self.long_message)
+        
+class Alisa(Generator):
+        
+    def get_randomly_polarization(self):
+        self.she_send_randomly_polarization=self.randomly_polarization
+        return self.she_send_randomly_polarization
+        
+    def get_randomly_bases(self):
+        self.she_send_randomly_bases=self.randomly_bases
+        return self.she_send_randomly_bases
+        
+class Bob(Generator):
+        
+    def get_randomly_polarization(self):
+        self.he_send_randomly_polarization=self.randomly_polarization
+        return self.he_send_randomly_polarization
+        
+    def get_randomly_bases(self):
+        self.he_send_randomly_bases=self.randomly_bases
+        return self.he_send_randomly_bases
+        
+        
+class Various_measurement(Alisa,Bob,Generator):
+    
+    def compare_Bob_Alise(self):
+        self.taken_qubits=[]
+        i=0
+        while i < self.long_message:
+            if self.she_send_randomly_bases[i] == self.he_send_randomly_bases[i]:
+                self.taken_qubits.append(self.she_send_randomly_polarization(i))
+            else:
+                self.taken_qubits.append(self.he_send_randomly_bases(i))
+        i=i+1
+        return self.taken_qubits
+        
+    def generate_key(self):
+        self.key=[]
+        i=0
+        while i < self.set_long:
+            if self.she_send_randomly_bases[i] == self.he_send_randomly_bases[i]:
+                if (-90)<=self.taken_qubits[i]<90:
+                    self.key.append(1)
+                else:
+                    self.key.append(0)
+            else:
+                pass
+    
+class Operation_Dice(Qubit):
     def set_the_qubit(self):
         if self.alfa==None and self.theta==None:
             self.set_polariz_for_qubit = np.random.randint([0,380])
@@ -27,7 +96,7 @@ class Qubit:
             angle_in_rad = math.radians((int(angle))/2)
             self.a = math.cos(angle_in_rad)
             self.b = math.sin(angle_in_rad)
-            self.circ = e**(turn.imag)
+            self.circ = math.e**(turn.imag)
             q1 = 0
             q2 = 1
             q3 = self.a
@@ -55,59 +124,22 @@ class Qubit:
             angle_in_rad = math.radians((int(angle))/2)
             self.a = math.cos(angle_in_rad)
             self.b = math.sin(angle_in_rad)
-            self.circ = e**(turn.imag)
+            self.circ = math.e**(turn.imag)
             q1 = 0
             q2 = 1
             q3 = self.a
             q4 = (self.circ)*(self.b)
             self.qubit = [[q1,q2],[q3,q4]]
-            
-#q=Qubit(45,0)
-#q.set_the_qubit()
-#print (q) 
-            
-class Generator(Qubit):
-    
-    def Alisa(self, number_bas=None,Long_message):
-        self.set_long = Long_message
-        self.bas = number_bas
+        return self.qubit
         
-    def Polarization(self,*args):
-        self.tuple_pol=tuple(args)
-        self.len_pol=len(self.tuple_pol)
-        return args
-    
-    def Bases(self,*args):
-        self.tuple_bas = tuple(args)
-        self.len_bas = len(self.tuple_bas)
-        return args 
-    
-    def Alise_create(self):
-        self.bas_Alisa=np.random.choice(self.tuple_bas,int(self.set_long),True,None)
-        self.go=np.random.choice(self.tuple_pol,int(self.set_long),True,None)
-    
-    def Bob_create(self):
-        self.Bobi=np.random.choice(self.tuple_pol,int(self.set_long),True,None)
-        self.Bob_pol=np.random.choice(self.tuple_pol,int(self.set_long),True,None)
         
-    def Bob_measures(self):
-        self.taken_qubits=[]
-        i=0
-        while i<int(self.set_long):
-            if self.bas_Alisa[i]==self.bas_Bob[i]:
-                self.taken_qubits.append(self.go(i))
-            else:
-                taken_qubits.append(self.Bob_pol(i))
-        return self.taken_qubits
         
-    def Key(self):
-        self.key=[]
-        i=0
-        while i<int(self.set_long):
-            if self.bas_Alisa[i]==self.bas_Bob[i]:
-                if (-90)<=self.taken_qubits[i]<90:
-                    self.key.append(1)
-                else:
-                    self.key.append(0)
-            else:
-                pass
+        
+a=Alisa(2,10)
+a.write_bases(0,1)
+a.write_polarization(0,45,33,135)
+a.send()
+a.get_randomly_polarization()
+print(a.she_send_randomly_polarization)
+print(a.she_send_randomly_polarization[1])
+
