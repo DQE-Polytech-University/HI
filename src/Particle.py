a76efa9de95cb803aabfbf99f41a013e87818514
Alena Popova <Popova-alena1995@mail.ru>
2016-05-31 02:05:10 +0400

    +Гейт

--- src/Particle.py
+++ src/Particle.py
@@ -59,72 +59,85 @@ class CNOT():
         
     def __repr__(self): 
         return "Gate_pi: " + str(self.apply_CNOT)
-
+        
+class Projection_gate():
+    
+    def __init__(self,*args,**kwargs):
+        i=0
+        mass_gate1=[]
+        mass_gate2=[]
+        pol1=args
+        pol2=kwargs
+        pol=np.random.choice([pol1,pol2])
+        while i<len(pol):
+            radiany=math.radians(int(pol[i]))
+            mass_gate1.append(math.cos(radiany))
+            mass_gate2.append(math.sin(radiany))
+            i=i+1
+        self.gate = [[mass_gate1],[mass_gate2]]
+    
+    def theta(self,*args):
+        thet=args
+        mass_gate1=[]
+        mass_gate2=[]
+        i=0
+        while i<len(thet):
+            b1 = complex(0,int(thet[i]))
+            mass_gate1.append(1)
+            mass_gate2.append((math.e**(b1.imag)))
+            i=i+1
+        self.gate_theta=[[mass_gate1],[mass_gate2]]
 
 
 class Actor():
     def __init__(self, quantity_bas,long_message):#This class
     #constructor generator. Set the number of bases used and the message length
-        self.long_message =long_message
+        self.long_message = long_message
         self.quantity_bas = quantity_bas
         
     def set_alpha1(self,*args):
     #We set the desired polarization. Example: 0, 45, 90, ...
-        self.write_alpha1=tuple(args)
-      #  self.len_pol=len(self.write_pol)
+        self.write_alpha1 = args
         return args
+        
     def set_alpha2(self,*args):
     #We set the desired polarization. Example: 0, 45, 90, ...
-        self.write_alpha2=tuple(args)
-      #  self.len_pol=len(self.write_pol)
+        self.write_alpha2 = args
         return args
         
     def write_theta(self,*args):
         #A predetermined angle in the other plane Bloch sphere 
     #to describe the first qubit by appropriate angles
-        self.write_theta=tuple(args)
-      #  self.len_theta=len(self.write_theta)
+        self.write_theta = args
         return args
     
     def write_bases(self,*args): 
         #We denote bases for themselves. This agreement
-        self.write_bas = tuple(args)
-      #  self.len_bas = len(self.write_bas)
+        self.write_bas = args
         return args
 
     def send(self):
         #This function is used for a random mixing bases and polarizations.
     #Their use Alice and Bob. But if you need something to mix, call this function.
-        i=0
+        a=0
+        randomly_bases=[]
         self.randomly_alpha=[]
         self.randomly_theta=[]
-        self.randomly_bases=np.random.choice(self.write_bas,self.long_message)
-        while i<self.long_message:
-            bas=self.randomly_bases[i]
-            a=0
-            while a<len(self.write_bas):
-                if bas==self.write_bas[a]:
-                    self.randomly_alpha.append=np.random.choice([self.write_alpha1[a],self.write_alpha2[a]])
-                else:
-                    pass
-                a=a+1
-            i=i+1
-        al=0
-        while al<self.long_message:
-            pol=self.randomly_alpha[al]
-            ap=0
-            while ap<self.len_pol:
-                if const==self.write_pol[a]:
-                    self.randomly_theta.insert(i,self.write_theta[a])
-                else:
-                    pass
-                a=a+1
-            i=i+1
-        return self.randomly_theta
-                    
-                
+        i=np.random.choice(int(self.write_bas),self.long_message)
+        while a<self.long_message:
+            c=[x*0 for x in range(len(self.write_bas))]
+            c[i[a]]=1
+            randomly_bases.append(c)
+        randomly_polarization=[]
+        l=0
+        while l<self.long_message:
+            g=Projection_gate(self.write_alpha1,self.write_alpha2)
+            qubit1=np.dot(randomly_bases[l],g.gate)
+            pol=math.acos(qubit1[1])
+            randomly_polarization.append(math.degrees(pol))
+            l=l+1
 
-class Alice(Generator):#Here they take their randomly values
+class Alice(Actor):#Here they take their randomly values
         
     def get_randomly_polarization(self):
         self.she_send_randomly_polarization=self.randomly_polarization
@@ -135,7 +148,7 @@ class Alice(Generator):#Here they take their randomly values
         return self.she_send_randomly_bases
     
         
-class Bob(Generator):
+class Bob(Actor):
         
     def get_randomly_polarization(self):
         self.he_send_randomly_polarization=self.randomly_polarization
@@ -147,7 +160,7 @@ class Bob(Generator):
         
 
         
-class Various_measurement(Alice,Bob,Generator,Operation_Dice):
+class Various_measurement(Alice,Bob,Actor):
     
     def compare_bob_alice(self): #Alice and Bob compare their bases and this
     #forms the following key. If the bases are equal to the true value,
@@ -175,27 +188,9 @@ class Various_measurement(Alice,Bob,Generator,Operation_Dice):
             else:
                 pass
             
-    def bring_qubits(self):#Generates a corresponding qubits. While randomly
-        self.my_qubits=[]
-        n=0
-        while n<self.long_message:
-            q=Operation_Dice(None,None)
-            self.my_qubits.append(q.qubit)
-            n=n+1
-        return self.my_qubits
+
         
-class Gate(Qubit):
-    
-    def transform_qubit(self):#qubit is transformed into a matrix 
-    #with the probabilities of its states
-        a = math.cos(self.alpha)
-        b2 = math.sin(self.alpha)
-        b1 = complex(0,self.theta)
-        b1 = math.e**(b1.imag)
-        b = b1*b2
-        self.qubit = [[a],[b]]
-        #It Hadamard gates, phase, NOT and pi-gate.
-        #They are needed for controlled action on the qubit 
+        
         
         
 
@@ -205,16 +200,5 @@ class Gate(Qubit):
         
         
         
-        
-a=Alice(2,10)
-a.write_bases(0,1)
-a.write_polarization(0,45,33,135)
-a.send()
-a.get_randomly_polarization()
-print(a.she_send_randomly_polarization)
-print(a.she_send_randomly_polarization[1])
 
-a=Various_measurement(2,10)
-a.bring_qubits()
-print(a.my_qubits)
 
